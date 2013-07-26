e78f7f100b8f558a0714b3e01f8cd585a87bd282
Marcos Gonçalves <marcos@visie.com.br>
2013-07-26 19:27:59 -0300

    Layout e fuções

--- scripts/functions.js
+++ scripts/functions.js
@@ -24,40 +24,10 @@
         to_down = 34;
         limit=108;
 
-
     $(document).ready(function() {
         $(".button").click(function() {
             mySound.play();
         });
-
-        $(".chat-abas li.global").click(function() {
-            $(".chat-abas li").removeClass("active");
-            $(this).addClass(" active");
-
-            $("#public").removeClass(" hidden");
-            $("#party").addClass(" hidden");
-            $("#guild").addClass(" hidden");
-        });
-
-        $(".chat-abas li.grupo").click(function() {
-            $(".chat-abas li").removeClass("active");
-            $(this).addClass(" active");
-
-            $("#party").removeClass(" hidden");
-            $("#public").addClass(" hidden");
-            $("#guild").addClass(" hidden");
-        });
-
-        $(".chat-abas li.guilda").click(function() {
-            $(".chat-abas li").removeClass("active");
-            $(this).addClass(" active");
-
-            $("#guild").removeClass(" hidden");
-            $("#party").addClass(" hidden");
-            $("#public").addClass(" hidden");
-        });
-
-
 
 
         $(".barra-control a.up").click(function() {
@@ -153,5 +123,32 @@
             else{
                     $("#poligon").attr("points", a+","+b+" "+c+","+d+" "+e+","+f+" "+g+","+h+" "+i+","+j+" "+k+","+l); 
                 }
+        });
+
+        $(".chat-abas li.global").click(function() {
+            $(".chat-abas li").removeClass("active");
+            $(this).addClass(" active");
+
+            $("#public").removeClass(" hidden");
+            $("#party").addClass(" hidden");
+            $("#guild").addClass(" hidden");
+        });
+
+        $(".chat-abas li.grupo").click(function() {
+            $(".chat-abas li").removeClass("active");
+            $(this).addClass(" active");
+
+            $("#party").removeClass(" hidden");
+            $("#public").addClass(" hidden");
+            $("#guild").addClass(" hidden");
+        });
+
+        $(".chat-abas li.guilda").click(function() {
+            $(".chat-abas li").removeClass("active");
+            $(this).addClass(" active");
+
+            $("#guild").removeClass(" hidden");
+            $("#party").addClass(" hidden");
+            $("#public").addClass(" hidden");
         });
     });
