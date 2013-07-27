b24ea4c4fa90e012db78a21113d78b5c7fb92c7b
Victor Haine <victor.haine@icloud.com>
2013-07-26 22:52:43 -0300

    Diretório ragnabrowser removido

--- ragnabrowser/js/autentication.js
+++
@@ -1,9 +0,0 @@
-function loginsucess() {
-	setTimeout("window.location='select.php'",5000);
-}
-function loginfail() {
-	setTimeout("window.location='login.php'",5000);
-}
-function logout() {
-	setTimeout("window.location='index.php'",5000);
-}
